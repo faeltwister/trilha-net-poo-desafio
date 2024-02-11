a6e668fbd7d2379b2897720c0e0cf29c1fb35275
faeltwister <faeltwister@gmail.com>
2024-02-11 20:36:20 -0300

    Implementado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone i1 = new("961399185","12S","5458-56D",512);
+
+Console.WriteLine("Iphone");
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Zap");
+
+Console.WriteLine("--------------");
+
+Nokia n1 = new("961355658","C55","9085-48A",128);
+Console.WriteLine("Nokia");
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Linkedin");
